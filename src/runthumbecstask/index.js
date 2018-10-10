bde667b7befce9ff24c7bf3c2e18ea0ad165c1dd
Rupak Ganguly <rupakg@gmail.com>
2018-10-10 01:11:32 -0400

    cleanup code.

--- src/runthumbecstask/index.js
+++ src/runthumbecstask/index.js
@@ -4,16 +4,13 @@
 const DOCKER_TASK_ARN = process.env.DOCKER_TASK_ARN;
 const DOCKER_TASK_SUBNETS = process.env.DOCKER_TASK_SUBNETS;
 const INPUT_BUCKET_NAME = process.env.BUCKET_NAME;
-// const INPUT_BUCKET_ARN = process.env.BUCKET_ARN;
 const OUTPUT_BUCKET_NAME = process.env.BUCKET_NAME_2;
-// const OUTPUT_BUCKET_ARN = process.env.BUCKET_ARN_2;
 
 const ECS_CLUSTER_NAME = process.env.ECS_CLUSTER_NAME || 'default';
 // arn:aws:ecs:us-east-1:account-id:task-definition/taskName:version
 const temp = DOCKER_TASK_ARN.split(':')
 const ECS_TASK_DEFINITION = `${temp[5].split('/')[1]}:${temp[6]}`;
-//const ECS_TASK_DEFINITION = 'stackery-156497753425579-dockerTaskA26298F2-XN7H6VY0F8W:1'; // process.env.ECS_TASK_DEFINITION;
-const OUTPUT_S3_PATH = process.env.OUTPUT_S3_PATH || OUTPUT_BUCKET_NAME // 'stackery-156497753425579-objectstore16b4761a';
+const OUTPUT_S3_PATH = OUTPUT_BUCKET_NAME;
 const OUTPUT_S3_AWS_REGION = process.env.OUTPUT_S3_AWS_REGION || 'us-east-1';
 
 const ecsApi = require('./ecs');
@@ -33,13 +30,11 @@ module.exports.handler = function handler (event, context, callback) {
   console.log(JSON.stringify(event));
   console.log(`A new video file '${key}' was uploaded to '${bucket}' for processing.`);
 
-  console.log("ENV: DOCKER_TASK_ARN", DOCKER_TASK_ARN);
-  console.log("ECS_TASK_DEFINITION", ECS_TASK_DEFINITION);
-  console.log("ENV: DOCKER_TASK_SUBNETS", DOCKER_TASK_SUBNETS);
-  console.log("ENV: INPUT_BUCKET_NAME", INPUT_BUCKET_NAME);
-  // console.log("ENV: INPUT_BUCKET_ARN", INPUT_BUCKET_ARN);
-  console.log("ENV: OUTPUT_BUCKET_NAME", OUTPUT_BUCKET_NAME);
-  // console.log("ENV: OUTPUT_BUCKET_ARN", OUTPUT_BUCKET_ARN);
+  // console.log("ENV: DOCKER_TASK_ARN", DOCKER_TASK_ARN);
+  // console.log("ECS_TASK_DEFINITION", ECS_TASK_DEFINITION);
+  // console.log("ENV: DOCKER_TASK_SUBNETS", DOCKER_TASK_SUBNETS);
+  // console.log("ENV: INPUT_BUCKET_NAME", INPUT_BUCKET_NAME);
+  // console.log("ENV: OUTPUT_BUCKET_NAME", OUTPUT_BUCKET_NAME);
 
   // parse the file processing details
   // video file: test_00-08.mp4
@@ -104,5 +99,4 @@ var runThumbnailGenerateTask = (s3_video_url, thumbnail_file, frame_pos) => {
   params.networkConfiguration.awsvpcConfiguration.subnets = docker_subnet_items;
 
   ecsApi.runECSTask(params);
-
 }
